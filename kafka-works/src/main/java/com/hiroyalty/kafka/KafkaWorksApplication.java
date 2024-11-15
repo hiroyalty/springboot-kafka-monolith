af058cd95c2016cb7a7219b01b4e7771041456e3
Gbolaga Famodun <gbolaga.famodun@gmail.com>
2024-11-15 17:33:43 -0500

    kafka init and configuration setup

---
+++ kafka-works/src/main/java/com/hiroyalty/kafka/KafkaWorksApplication.java
@@ -0,0 +1,13 @@
+package com.hiroyalty.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaWorksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaWorksApplication.class, args);
+	}
+
+}
